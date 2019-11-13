eefddf0c65fd3309966b1c60d8df952092ebc1ab
Hunter Metts <hunter.metts@owentechs.com>
2019-11-13 09:52:46 -0600

    setup and first page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import InputRange from 'react-input-range';
-import { Grid, Typography, Card, CardContent, Button,
-  IconButton, Dialog, DialogTitle, DialogContent, DialogContentText,
-  DialogActions, TextField } from '@material-ui/core';
+import { Grid,  Dialog, DialogContent, DialogTitle, FormControlLabel, Checkbox, FormLabel, FormGroup, Button, Typography, DialogActions } from '@material-ui/core';
 import "react-input-range/lib/css/index.css"
 import './App.css';
 
@@ -13,133 +11,117 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      collegeName:'University of Tennessee - Knoxville',
-      isLoading: true,
-      collegeTuitionPriceInState:12724,
-      collegeTuitionPriceOutOfState:31144,
-      GPAValue: 0.00,
-      ScholarshipValue: 0,
-      ACTValue: 0,
-      yourCostOpen: false,
-      yourCostValue:"0"
+      civicsTitle:'Civics Exam',
+      civicsTestOpen: false,
+      page:1,
+      currentPage:0,
     }
   }
 
- calculateTuition(){
-  if(this.state.ACTValue > 29 && this.state.GPAValue > 3.8){
-    this.setState({
-      collegeTuitionPriceInState:9724
-    })
-  } else {
-    this.setState({
-      collegeTuitionPriceInState:12724
-    })
-  }
- }
-
 handleOpen = () => {
   this.setState({
-    yourCostOpen:true
+    civicsTestOpen:true
   })
 };
-
-handleACT = (value) => {
-  this.setState({
-    ACTValue:value
-  })
-  this.calculateTuition()
-}
 
 handleClose = () => {
   this.setState({
-    yourCostOpen:false
+    civicsTestOpen:false
   })
 };
+
+onNextClick() {
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+      page: this.state.page + 1
+    })
+}
+
+renderQuestions() {
+
+  const questions = [
+    {id: 1, title:"Who lived in America before the Europeans arrived?", answer1:'Chinese', answer2:'Russians', answer3:'Pacific Islanders', answer4:'American Indians'},
+    {id: 2, title:"Who was the first President of the United States?", answer1:'John Hancock', answer2:'James Madison', answer3:'Benjamin Franklin', answer4:'George Washington'},
+    {id: 3, title:"What was one important thing that Abraham Lincoln did?", answer1:'Led the U.S. during the Civil War', answer2:'Led the U.S. during the War of 1812', answer3:'Led the U.S. during the American Revolution', answer4:'Led the U.S. during the Mexican-American War'},
+    {id: 4, title:"When is Independence Day celebrated?", answer1:'April 19th', answer2:'July 4th', answer3:'September 17th', answer4:'November 11th'},
+    {id: 5, title:'Who is the "Father of Our Country?"', answer1:'Abraham Lincoln', answer2:'Thomas Jefferson', answer3:'Benjamin Franklin', answer4:'George Washington'},
+    {id: 6, title:" What Ocean is on the East Coast of the United States?", answer1:'Arctic Ocean', answer2:'Indian Ocean', answer3:'Pacific Ocean', answer4:'Atlantic Ocean'},
+    {id: 7, title:"What did Martin Luther King, Jr. do?", answer1:'Fought for civil rights', answer2:'Became the first black U.S. Senator', answer3:'established the Harvard Law School', answer4:'Ran for President of the United States'},
+    {id: 8, title:"Which of the following is one of the two longest rivers in the United States?", answer1:'Ohio River', answer2:'Potomac River', answer3:'Colorado', answer4:'Mississippi River'},
+    {id: 9, title:"What do Americans show loyalty to when saying the Pledge of Allegiance?", answer1:'The state where you live', answer2:'The United States of America', answer3:'The Declaration of Independence', answer4:'The President of the United States of America'},
+    {id: 10, title:"Why did the colonists fight the British?", answer1:'Because of high taxes', answer2:'Because the British invaded France', answer3:'Because the colonists wanted a new king', answer4:'Because the colonists wanted religious freedom'},
+  ]
+
+  return (
+
+    <div>
+      <Grid container spacing={12} style={{marginTop:10}}>
+
+            <Grid container style={{marginBottom:10, padding:20}}>
+            {
+              questions.map((item, key) => {
+                return (
+                  <Grid key={key} item xs={6} style={{marginBottom:10, padding:20}}>
+                    <FormLabel component="legend">{item.id + ". " + item.title}</FormLabel>
+                    <FormGroup>
+                      <FormControlLabel key={key} 
+                      // onClick={() => this.onChoiceSelect(item, 1)}
+                          control={
+                              <Checkbox color='default' />
+                          }
+                          label={item.answer1}
+                      />
+                     <FormControlLabel key={key} 
+                      // onClick={() => this.onChoiceSelect(item, 1)}
+                          control={
+                              <Checkbox color='default' />
+                          }
+                          label={item.answer2}
+                      />
+                     <FormControlLabel key={key} 
+                      // onClick={() => this.onChoiceSelect(item, 1)}
+                          control={
+                              <Checkbox color='default' />
+                          }
+                          label={item.answer3}
+                      />
+                     <FormControlLabel key={key} 
+                      // onClick={() => this.onChoiceSelect(item, 1)}
+                          control={
+                              <Checkbox color='default' />
+                          }
+                          label={item.answer4}
+                      />
+                      
+                      </FormGroup>
+                  </Grid>
+                )
+              })
+            }
+            </Grid>
+
+          </Grid>
+    </div>
+  )
+}
   
 
 render() {
-
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0
-  })
-
-const collegePriceInState = this.state.collegeTuitionPriceInState
-const collegePriceInStateFormated = formatter.format(collegePriceInState)
-
-const collegePriceOutOfState = this.state.collegeTuitionPriceOutOfState
-const collegePriceOutOfStateFormated = formatter.format(collegePriceOutOfState)
-
-const collegeMaxScholarship = formatter.format(75000)
-
   return  (
     <div className="App">
       <header className="App-header">
         <p>
-          College Calculator
+          {this.state.civicsTitle}
         </p>
 
-        <p style={{fontSize:30}}>
-          {this.state.collegeName} <br></br> In-state Tuition: {collegePriceInStateFormated}
+        <p style={{fontSize:20}}>
+          The following exam in composed of 100 multiple choice questions. <br></br>You must get a 60% or higher to pass the exam and graduate, but you have as many tries as you need. 
         </p>
-        {/* <p style={{fontSize:30}}>
-          Out-of-state Tuition: {collegePriceOutOfStateFormated}
-        </p> */}
-
 
         <div style={{width:'60%', margin:20}}>
 
-            
-            <label className='labels'>Your GPA</label> 
-            <div style={{marginTop:20}}>
-              <InputRange
-                step={.01}
-                maxValue={4.00}
-                minValue={0.00}
-                formatLabel={GPAValue => GPAValue.toFixed(2)}
-                value={this.state.GPAValue}
-                onChange={value => this.setState({ GPAValue:value })}
-                // onChangeComplete={GPAValue => console.log("GPA: " + GPAValue)} 
-                />
-            </div>
-
-            <br></br>
-
-            <label className='labels'>
-              Your ACT
-            </label>
-            <div style={{marginTop:20}}>
-              <InputRange
-                step={1}
-                maxValue={36}
-                minValue={0}
-                formatLabel={ACTValue => ACTValue}
-                value={this.state.ACTValue}
-                onChange={value => this.handleACT(value)}
-                // onChangeComplete={ACTValue => console.log("ACT: " + ACTValue)}
-                />
-            </div>
-
-            <br></br>
-
-            <label className='labels'>
-              Any additional scholorships, grants, or savings
-            </label>
-            <div style={{marginTop:20}}>
-              <InputRange
-                step={1}
-                maxValue={75000}
-                minValue={0}
-                formatLabel={ScholarshipValue => formatter.format(ScholarshipValue)}
-                value={this.state.ScholarshipValue}
-                onChange={value => this.setState({ ScholarshipValue:value })}
-                // onChangeComplete={ScholarshipValue => console.log("Scholarship: " + ScholarshipValue)}
-                />
-            </div>
-
-            <button style={{marginTop:15}} onClick={this.handleOpen}>
-              Calculate
+            <button style={{marginTop:15, paddingRight:20, paddingLeft:20, fontWeight:'bold'}} onClick={this.handleOpen}>
+              Begin
             </button>
 
         </div>
@@ -149,18 +131,26 @@ const collegeMaxScholarship = formatter.format(75000)
    <Dialog 
       onClose={this.handleClose} 
       aria-labelledby="simple-dialog-title"
-      open={this.state.yourCostOpen}
-      maxWidth='sm'
+      open={this.state.civicsTestOpen}
+      maxWidth="md"
       fullWidth={true}
        >
-      <DialogTitle id="simple-dialog-title">Your college cost for {this.state.collegeName}:</DialogTitle>
+      <DialogTitle id="simple-dialog-title">Civics Exam</DialogTitle>
       <DialogContent>
-        {this.state.GPAValue}
-        <br></br>
-        {this.state.ACTValue}
-        <br></br>
-        {this.state.ScholarshipValue}
+        {this.renderQuestions()}
       </DialogContent>
+
+      <DialogActions>
+        <Grid item xs={8} style={{textAlign:'center'}}>
+                  <Typography style={{fontSize:16, marginTop:10, fontWeight:'bold'}}>{this.state.page} / 10</Typography>
+        </Grid>
+        <Grid item xs={2} style={{textAlign:'right'}}>
+          <Button onClick={() => this.onNextClick()} style={{backgroundColor:'#FFF', marginRight:10}}>
+              <Typography style={{color:'#000', fontSize:14, fontWeight:'bold'}}>Next</Typography>
+          </Button>
+        </Grid>
+      </DialogActions>
+
     </Dialog>
 
     </div>
